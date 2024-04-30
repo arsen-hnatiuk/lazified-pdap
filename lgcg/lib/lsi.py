db62b3be72afb5b22d5e02bda3adbe93f133bc23
arsen-hnatiuk <arsen.hnatiuk@hu-berlin.de>
2024-04-30 17:25:20 +0200

    feat: improved measures and implemented global search

--- lgcg/lib/lsi.py
+++ lgcg/lib/lsi.py
@@ -1,2 +1,13 @@
+import numpy as np
+from typing import Callable
+
+
 class LSI:
-    pass
+    # Implementation of the local support improver
+    def __init__(
+        self, active_set: np.ndarray, p: Callable, tol: float, gap: float
+    ) -> None:
+        self.active_set = active_set
+        self.p = p
+        self.tol = tol
+        self.gap = gap
--- lgcg/lib/measure.py
+++ lgcg/lib/measure.py
@@ -3,22 +3,20 @@ from typing import Callable
 
 
 class Measure:
-    # An implementation of a class that represents measures on Omega
+    # An implementation of a class that represents finitely supported measures on Omega
     def __init__(
         self,
         support: np.ndarray = np.array([]),
         coefficients: np.ndarray = np.array([]),
     ) -> None:
-        self.support = np.unique(support, axis=0)
-        self.coefficients = coefficients
+        self.support = np.unique(np.array(support), axis=0)
+        self.coefficients = np.array(coefficients)
         assert len(self.support) == len(
             self.coefficients
         ), "The support and coefficients must have the same length"
 
-    def multiply(self, constant: float) -> None:
-        self.coefficients *= constant
-
     def add_zero_support(self, support_plus: np.ndarray) -> None:
+        # Given an array of points, add them to the support with coefficient 0
         for point in support_plus:
             if point not in self.support:
                 if len(self.support.shape) == 1:
@@ -28,16 +26,30 @@ class Measure:
                 self.coefficients = np.append(self.coefficients, 0)
 
     def duality_pairing(self, fct: Callable) -> float:
+        # Compute the duality pairing of the measure with a function defined on Omega
         return sum([c * fct(x) for x, c in zip([self.support, self.coefficients])])
 
+    def __add__(self, other):
+        # Add two measures
+        new = Measure(
+            support=self.support.copy(), coefficients=self.coefficients.copy()
+        )
+        for x, c in zip(other.support, other.coefficients):
+            changed = False
+            for i, pos in enumerate(new.support):
+                if pos == x:
+                    new.coefficients[i] += c
+                    changed = True
+                    break
+            # new support point
+            if not changed:
+                new.add_zero_support(np.array([x]))
+                new.coefficients[-1] = c
+        return new
 
-def add_measures(u: Measure, v: Measure) -> Measure:
-    new = Measure(support=u.support, coefficients=u.coefficients)
-    for x, c in zip(v.support, v.coefficients):
-        for i, pos in enumerate(new.support):
-            if pos == x:
-                new.coefficients[i] += c
-                break
-        # new support point
-        new.add_zero_support(np.array([x]))
-    return new
+    def __mul__(self, other):
+        # Multiply a measure by a scalar
+        new = Measure(
+            support=self.support.copy(), coefficients=self.coefficients.copy() * other
+        )
+        return new
