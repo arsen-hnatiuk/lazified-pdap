008bce3bf3ca52026d187d094ba7d2869ee3f40b
arsen-hnatiuk <arsen.hnatiuk@hu-berlin.de>
2025-01-16 17:29:30 +0100

    feat: new newton implementation

--- lgcg/lgcg.py
+++ lgcg/lgcg.py
@@ -15,7 +15,7 @@ class LGCG:
     def __init__(
         self,
         target: np.ndarray,
-        k: Callable,
+        kappa: Callable,
         g: Callable,
         f: Callable,
         p: Callable,
@@ -33,11 +33,12 @@ class LGCG:
         m: float,
         bar_m: float,
         L: float,
+        L_H: float,
         R: float,
         global_search_resolution: float = 10,
     ) -> None:
         self.target = target
-        self.k = k
+        self.kappa = kappa
         self.f = f
         self.p = p
         self.grad_P = grad_P
@@ -51,10 +52,12 @@ class LGCG:
         self.m = m
         self.bar_m = bar_m
         self.L = L
+        self.L_H = L_H
         self.norm_K_star = norm_K_star
         self.norm_K_star_L = norm_K_star_L
         self.Omega = Omega  # Example [[0,1],[1,2]] for [0,1]x[1,2]
         self.j = lambda u: self.f(u) + self.g(u.coefficients)
+        self.j_tilde = lambda pos, coef: self.f(Measure(pos, coef)) + self.g(coef)
         self.u_0 = Measure()
         self.M = min(self.j(self.u_0) / self.alpha, 10)  # Bound on the norm of iterates
         self.C = 4 * self.L * self.M**2 * self.norm_K_star**2
@@ -355,7 +358,9 @@ class LGCG:
                     u_plus = u_plus_tilde * 1
                     choice = "Step: GCG,"
                 # Peform SSN
-                K_support = np.transpose(np.array([self.k(x) for x in u_plus.support]))
+                K_support = np.transpose(
+                    np.array([self.kappa(x) for x in u_plus.support])
+                )
                 ssn = SSN(
                     K=K_support,
                     alpha=self.alpha,
@@ -514,7 +519,7 @@ class LGCG:
             v_k = Measure(support=np.array([x]), coefficients=[1])
             eta = min(1, self.explicit_Phi(p_u, u, v_k) / self.C)
             u_plus = u * (1 - eta) + v_k * eta
-            K_support = np.transpose(np.array([self.k(x) for x in u_plus.support]))
+            K_support = np.transpose(np.array([self.kappa(x) for x in u_plus.support]))
             ssn = SSN(K=K_support, alpha=self.alpha, target=self.target, M=self.M)
             u_raw = ssn.solve(tol=self.machine_precision, u_0=u_plus.coefficients)
             u_raw[np.abs(u_raw) < self.machine_precision] = 0
@@ -566,11 +571,19 @@ class LGCG:
                         coefficient += c_local
                         already_clustered.append(x_local)
                 clustered_tuples.append((x, coefficient))
-        return clustered_tuples
+        points, coefs = tuple(zip(*clustered_tuples))
+        points = list(points)
+        coefs = list(coefs)
+        return points, coefs
 
-    def compute_newton_step(
-        self, points: np.ndarray, coefs: np.ndarray, damped: bool
-    ) -> Measure:
+    def newton_step(
+        self, points: np.ndarray, coefs: np.ndarray, beta: float, damped: bool
+    ) -> tuple:
+        points_init = points.copy()
+        coefs_init = coefs.copy()
+        if any(np.abs(coefs) < beta):
+            # Outside of the domain of j_tilde
+            return points, coefs
         grad_j_z = self.grad_j(points, coefs)
         hess_j_z = self.hess_j(points, coefs)
         if damped:
@@ -580,14 +593,22 @@ class LGCG:
                     -self.m
                     + np.sqrt(
                         self.m**2
-                        + 8 * self.m * self.L * self.bar_m**3 * np.linalg.norm(grad_j_z)
+                        + 32
+                        * self.m
+                        * self.L_H
+                        * self.bar_m**3
+                        * np.linalg.norm(grad_j_z)
                     )
                 )
-                / (4 * self.L * self.bar_m**3 * np.linalg.norm(grad_j_z)),
+                / (16 * self.L_H * self.bar_m**3 * np.linalg.norm(grad_j_z)),
             )
+            nu = nu**0.2
         else:
             nu = 1
-        update_direction = np.linalg.solve(hess_j_z, grad_j_z)
+        try:
+            update_direction = np.linalg.solve(hess_j_z, grad_j_z)
+        except np.linalg.LinAlgError:
+            update_direction = np.zeros_like(grad_j_z)
         # Transform vector into tuples
         coefs -= nu * update_direction[-len(coefs) :]
         for i, point in enumerate(points):
@@ -598,10 +619,69 @@ class LGCG:
                 ]
             )
             points[i] = self.project_into_omega(new_point)
-        u_plus = Measure(points, coefs)
-        return u_plus
+        return points, coefs
 
-    def solve_newton(self, tol: float, merging_frequency=5, damped=True) -> tuple:
+    def lgcg_step(self, p_u: Callable, u: Measure, epsilon: float) -> tuple:
+        if self.explicit_Phi(p_u, u, Measure()) >= self.M * epsilon:
+            v_k = Measure()
+            global_valid = True
+        else:
+            x_k, global_valid = self.global_search(u, epsilon)
+            if abs(p_u(x_k)) < self.alpha:
+                v_k = Measure()  # The actual maximizer is v_k=0
+            else:
+                v_k = Measure([x_k], [self.M * np.sign(p_u(x_k))])
+        eta = min(1, self.explicit_Phi(p_u, u, v_k) / self.C)
+        if not global_valid:
+            # We have a global maximum x_k
+            u_plus = u * (1 - eta) + v_k * eta
+            epsilon = 0.5 * self.explicit_Phi(p_u, u, v_k) / self.M
+        else:
+            u_plus = u * (1 - eta) + v_k * eta
+        return u_plus, epsilon, global_valid
+
+    def low_dimensional_step(self, u_plus, Psi) -> Measure:
+        K_support = np.transpose(np.array([self.kappa(x) for x in u_plus.support]))
+        ssn = SSN(K=K_support, alpha=self.alpha, target=self.target, M=self.M)
+        u_raw = ssn.solve(tol=Psi, u_0=u_plus.coefficients)
+        u_raw[np.abs(u_raw) < self.machine_precision] = 0
+        # Reconstruct u
+        u = Measure(
+            support=u_plus.support[u_raw != 0].copy(),
+            coefficients=u_raw[u_raw != 0].copy(),
+        )
+        return u
+
+    def get_regularity_inequalities(
+        self, points, coefs, points_plus, coefs_plus, epsilon_plus, tol
+    ) -> tuple:
+        grad_j_z = self.grad_j(points, coefs)
+        norm_grad = np.linalg.norm(grad_j_z)
+        hess_j_z = self.hess_j(points, coefs)
+        j_tilde_diff = self.j_tilde(points_plus, coefs_plus) - self.j_tilde(
+            points, coefs
+        )
+        ineq73 = j_tilde_diff <= -0.125 * self.m * norm_grad**2
+        # logging.info(f"diff: {j_tilde_diff}, grad_norm: {norm_grad}")
+        try:
+            ineq75 = (
+                np.linalg.norm(np.linalg.solve(hess_j_z, grad_j_z)) / norm_grad
+                <= 2 * self.bar_m
+            )
+        except np.linalg.LinAlgError:
+            ineq75 = False
+        if self.M * epsilon_plus <= self.C:
+            ineq78 = norm_grad**2 >= self.M**2 * epsilon_plus**2 / (
+                2 * self.bar_m * self.C
+            )
+        else:
+            ineq78 = norm_grad**2 >= (2 * self.M * epsilon_plus - self.C) / (
+                2 * self.bar_m
+            )
+        tol_ineq = 2 * self.M * epsilon_plus >= tol
+        return ineq73, ineq75, ineq78, tol_ineq
+
+    def solve_newton_old(self, tol: float, merging_frequency=5, damped=True) -> tuple:
         u = Measure()
         p_u = self.p(u)
         epsilon = 0.5 * self.j(u) / self.M
@@ -691,7 +771,7 @@ class LGCG:
                 steps_since_merging += 1
 
             # Low-dimensional step
-            K_support = np.transpose(np.array([self.k(x) for x in u_plus.support]))
+            K_support = np.transpose(np.array([self.kappa(x) for x in u_plus.support]))
             ssn = SSN(K=K_support, alpha=self.alpha, target=self.target, M=self.M)
             u_raw = ssn.solve(tol=local_Psi, u_0=u_plus.coefficients)
             u_raw[np.abs(u_raw) < self.machine_precision] = 0
@@ -717,3 +797,117 @@ class LGCG:
         c_points, c_coefs = tuple(zip(*self.local_merging(u, p_u)))
         u = Measure(c_points, c_coefs)
         return u, grad_norms, times, supports, objective_values
+
+    def solve_newton(
+        self, tol: float, lgcg_frequency: int = 5, beta: float = 1e-5, damped=True
+    ) -> tuple:
+        u = Measure()
+        p_u = self.p(u)
+        epsilon = 0.5 * self.j(u) / self.M
+        Psi = self.gamma * self.sigma / (16 * self.norm_K_star**2 * self.L**2)
+        k = 1
+        initial_time = time.time()
+        times = [time.time() - initial_time]
+        supports = [0]
+        objective_values = [self.j(u)]
+        while True:
+            # LocalRoutine
+            if len(u.coefficients):
+                s = 1
+                points, coefs = self.local_merging(u, p_u)
+                u_ks = Measure(points, coefs)
+                p_u_ks = self.p(u_ks)
+                epsilon_ks = epsilon + 0.5 * (self.j(u_ks) - self.j(u)) / self.M
+                points_plus, coefs_plus = self.newton_step(
+                    points.copy(), coefs.copy(), beta, damped
+                )
+                u_ks_plus = Measure(points_plus, coefs_plus)
+                u_hat_plus, epsilon_ks_plus, global_valid = self.lgcg_step(
+                    p_u_ks, u_ks, epsilon_ks
+                )
+
+                # Regularity conditions
+                ineq73, ineq75, ineq78, tol_ineq = self.get_regularity_inequalities(
+                    points, coefs, points_plus, coefs_plus, epsilon_ks_plus, tol
+                )
+
+                # evs = np.linalg.eig(self.hess_j(points, coefs)).eigenvalues
+                # inv_evs = [1 / ev for ev in evs]
+                # logging.info(f"min: {min(inv_evs)}, max: {max(inv_evs)}")
+                logging.info(f"{ineq73}, {ineq75}, {ineq78}, {tol_ineq}")
+                while ineq73 and ineq75 and ineq78 and tol_ineq:
+                    times.append(time.time() - initial_time)
+                    supports.append(len(u_ks.support))
+                    objective_values.append(self.j(u_ks))
+                    logging.info(
+                        f"{k}, {s}: lazy: {global_valid}, support: {u_ks.support}, coefs: {u_ks.coefficients}, epsilon: {epsilon_ks_plus}, objective: {self.j(u_ks):.3E}"
+                    )
+
+                    s += 1
+                    points, coefs = points_plus.copy(), coefs_plus.copy()
+                    u_ks = u_ks_plus * 1
+                    p_u_ks = self.p(u_ks)
+                    points_plus, coefs_plus = self.newton_step(
+                        points.copy(), coefs.copy(), beta, damped
+                    )
+                    u_ks_plus = Measure(points_plus, coefs_plus)
+                    if s % lgcg_frequency == 0:
+                        epsilon_ks = epsilon_ks_plus
+                        u_hat_plus, epsilon_ks_plus, global_valid = self.lgcg_step(
+                            p_u_ks, u_ks, epsilon_ks
+                        )
+                    ineq73, ineq75, ineq78, tol_ineq = self.get_regularity_inequalities(
+                        points, coefs, points_plus, coefs_plus, epsilon_ks_plus, tol
+                    )
+                    # evs = np.linalg.eig(self.hess_j(points, coefs)).eigenvalues
+                    # inv_evs = [1 / ev for ev in evs]
+                    # logging.info(f"min: {min(inv_evs)}, max: {max(inv_evs)}")
+                    logging.info(f"{ineq73}, {ineq75}, {ineq78}, {tol_ineq}")
+
+                # Choose best iterate so far
+                all_iterates = [u, u_ks, u_ks_plus, u_hat_plus]
+                objective_values = [self.j(iterate) for iterate in all_iterates]
+                choice_index = np.argmin(objective_values)
+                u_prime = all_iterates[choice_index] * 1
+                if choice_index:
+                    epsilon = epsilon_ks_plus
+                if not choice_index:
+                    logging.info(objective_values)
+            else:
+                choice_index = 0
+                u_prime = u * 1
+            p_u_prime = self.p(u_prime)
+
+            if not choice_index:
+                # u'=u, no improvement yet
+                u_plus, epsilon, global_valid = self.lgcg_step(
+                    p_u_prime, u_prime, epsilon
+                )
+            else:
+                # LGCG has already been computed
+                u_plus = u_prime * 1
+
+            # Termination criterium
+            if 2 * self.M * epsilon < tol:
+                u = u_prime * 1
+                break
+
+            u = self.low_dimensional_step(u_plus, Psi)
+            Psi = max(Psi / 2, self.machine_precision)
+            p_u = self.p(u)
+
+            times.append(time.time() - initial_time)
+            supports.append(len(u.support))
+            objective_values.append(self.j(u))
+            logging.info(
+                f"{k}: choice: {choice_index}, lazy: {global_valid}, support: {u.support}, epsilon: {epsilon}, objective: {self.j(u):.3E}"
+            )
+            k += 1
+
+        times.append(time.time() - initial_time)
+        supports.append(len(u.support))
+        objective_values.append(self.j(u))
+        logging.info(
+            f"{k}: choice: {choice_index}, lazy: {global_valid}, support: {u.support}, epsilon: {epsilon}, objective: {self.j(u):.3E}"
+        )
+        return u, times, supports, objective_values
